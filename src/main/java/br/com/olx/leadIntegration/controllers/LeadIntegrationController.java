3f3fd8064c66396c0058e8dab4aec4c4604bf915
kennedyrocha <kennedy279972@gmail.com>
2022-06-21 17:46:12 -0400

    first commit

---
+++ src/main/java/br/com/olx/leadIntegration/controllers/LeadIntegrationController.java
@@ -0,0 +1,20 @@
+package br.com.olx.leadIntegration.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import br.com.olx.leadIntegration.models.Lead;
+
+@RestController
+@RequestMapping("/leads")
+public class LeadIntegrationController {
+    
+    @PostMapping("/lead")
+    public String lead(@RequestBody Lead lead){
+        return "{\"message\": \"Lead successfully received\"}";
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+	public String mainPage() {
+		
+		return "This is working!";
+	}
+}
