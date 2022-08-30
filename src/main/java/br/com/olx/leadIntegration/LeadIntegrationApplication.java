5844f590a226ed1eec082d52a686897d6c7b7212
kennedy rocha <kennedy279972@gmail.com>
2022-08-30 17:19:41 -0400

    XML Encode

--- src/main/java/br/com/olx/leadIntegration/LeadIntegrationApplication.java
+++ src/main/java/br/com/olx/leadIntegration/LeadIntegrationApplication.java
@@ -3,6 +3,12 @@ package br.com.olx.leadIntegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class LeadIntegrationApplication {
 
@@ -10,4 +16,11 @@ public class LeadIntegrationApplication {
 		SpringApplication.run(LeadIntegrationApplication.class, args);
 	}
 
+    @Bean
+    public MappingJackson2XmlHttpMessageConverter mappingJackson2XmlHttpMessageConverter(Jackson2ObjectMapperBuilder builder) {
+        XmlMapper xmlMapper = builder.createXmlMapper(true).build();
+         
+        xmlMapper.configure(ToXmlGenerator.Feature.WRITE_XML_DECLARATION, true);
+        return new MappingJackson2XmlHttpMessageConverter(xmlMapper);
+    }
 }
